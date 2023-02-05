9bed914ff1d0ce332fa1828e7639fb70fd8c5262
kumarapraveena <prabhupraveen958@gmail.com>
2023-02-05 16:27:06 +0530

    addingcontent in header

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div>
         <navbar style={{display:"flex",justifyContent:"space-between"}}>
-            <Link to="/add">AddProduct</Link>
+            <Link to="/add">AddingProduct</Link>
             <Link to="/update">UpdateProduct</Link>
             <Link to="/login">login</Link>
             <Link to="/register">Register</Link>
